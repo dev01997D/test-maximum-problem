bb8266385427df86368a0de58a25519cf07d03a7
dev01997D <devnandan01997@gmail.com>
2020-10-09 10:02:55 +0530

    Refactored: Refactored the code to find max object from given three Objects at any position refactored

--- main/java/com/blz/testmaximum/runner/TestMaximumMain.java
+++ main/java/com/blz/testmaximum/runner/TestMaximumMain.java
@@ -4,49 +4,22 @@
 package com.blz.testmaximum.runner;
 
 public class TestMaximumMain {
-	Integer a, b, c;
 
 	public TestMaximumMain() {
 	}
 
-	// Printing the largest number
-	public void printMaxNumber() {
-		System.out.println("The maximum number is : " + findMaxInteger(a, b, c));
-	}
-
-	// To find Maximum integer number from input
-	public Integer findMaxInteger(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	// To find Maximum float number from input
-	public Float findMaxFloat(Float f1, Float f2, Float f3) {
-		Float max = f1;
-		if (f2.compareTo(max) > 0)
-			max = f2;
-		if (f3.compareTo(max) > 0)
-			max = f3;
-		return max;
-	}
-
-	// To find Maximum String from input
-	public String findMaxString(String s1, String s2, String s3) {
-		String max = s1;
-		if (s2.compareTo(max) > 0)
-			max = s2;
-		if (s3.compareTo(max) > 0)
-			max = s3;
+	public static <E extends Comparable<E>> E findMax(E obj1, E obj2, E obj3) {
+		E max = obj1;
+		if (obj2.compareTo(max) > 0)
+			max = obj2;
+		if (obj3.compareTo(max) > 0)
+			max = obj3;
 		return max;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Max Integer number is : " + new TestMaximumMain().findMaxInteger(78, 89, 15));
-		System.out.println("Max Float number is : " + new TestMaximumMain().findMaxFloat(8.9f, 45.2f, 40.6f));
-		System.out.println("Max String is : " + new TestMaximumMain().findMaxString("PineApple", "Apple", "Wanana"));
+		System.out.println("Max Integer number is : " + findMax(78, 89, 15));
+		System.out.println("Max Float number is : " + findMax(8.9f, 45.2f, 40.6f));
+		System.out.println("Max String is : " + findMax("PineApple", "Apple", "Wanana"));
 	}
 }
--- test/java/com/blz/testmaximum/tester/TestMaximumTest.java
+++ test/java/com/blz/testmaximum/tester/TestMaximumTest.java
@@ -11,58 +11,58 @@ public class TestMaximumTest {
 	@Test
 	public void test_Given_MaxInteger_At1st_Position_Returns_Same_Number() {
 		Integer actual = 65;
-		Integer expected = new TestMaximumMain().findMaxInteger(65, 45, 60);
+		Integer expected = TestMaximumMain.findMax(65, 45, 60);
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void test_Given_MaxInteger_At_2nd_Position_Returns_Same_Number() {
 		Integer actual = 96;
-		Integer expected = new TestMaximumMain().findMaxInteger(65, 96, 40);
+		Integer expected = TestMaximumMain.findMax(65, 96, 40);
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void test_Given_MaxInteger_At_3rd_Position_Returns_Same_Number() {
 		Integer actual = 115;
-		Integer expected = new TestMaximumMain().findMaxInteger(65, 96, 115);
+		Integer expected = TestMaximumMain.findMax(65, 96, 115);
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void test_Given_Max_Float_At_1st_Position_Returns_Same_Number() {
 		Float actual = 11.5f;
-		Float expected = new TestMaximumMain().findMaxFloat(11.5f, 8.6f, 5.9f);
+		Float expected = TestMaximumMain.findMax(11.5f, 8.6f, 5.9f);
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
 	public void test_Given_Max_Float_At_2nd_Position_Returns_Same_Number() {
 		Float actual = 45.9f;
-		Float expected = new TestMaximumMain().findMaxFloat(11.5f, 45.9f, 30.2f);
+		Float expected = TestMaximumMain.findMax(11.5f, 45.9f, 30.2f);
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
 	public void test_Given_Max_Float_At_3rd_Position_Returns_Same_Number() {
 		Float actual = 95.6f;
-		Float expected = new TestMaximumMain().findMaxFloat(70.6f, 32.5f, 95.6f);
+		Float expected = TestMaximumMain.findMax(70.6f, 32.5f, 95.6f);
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
 	public void test_Given_Max_String_At_1st_Position_Returns_Same_Number() {
 		String actual = "Manohar";
-		String expected = new TestMaximumMain().findMaxString("Manohar", "Apple", "Dev");
+		String expected = TestMaximumMain.findMax("Manohar", "Apple", "Dev");
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
 	public void test_Given_Max_String_At_2nd_Position_Returns_Same_Number() {
 		String actual = "Ram";
-		String expected = new TestMaximumMain().findMaxString("Manohar", "Ram", "Dev");
+		String expected = TestMaximumMain.findMax("Manohar", "Ram", "Dev");
 		Assert.assertEquals(expected, actual);
 	}
 	@Test
 	public void test_Given_Max_String_At_3rd_Position_Returns_Same_Number() {
 		String actual = "Wangdu";
-		String expected = new TestMaximumMain().findMaxString("Manohar", "Ram", "Wangdu");
+		String expected = TestMaximumMain.findMax("Manohar", "Ram", "Wangdu");
 		Assert.assertEquals(expected, actual);
 	}
 }
